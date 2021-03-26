98893ee4608feff2746851b3f54fb92417d14104
SaintEed <dsteed18@gmail.com>
2021-03-26 14:59:15 +0000

    Init

---
+++ thymeleaf-crud-db/src/test/java/com/deusnex/springboot/thymeleafdemo/ThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusnex.springboot.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
