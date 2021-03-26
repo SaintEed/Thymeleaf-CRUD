27ce9b72bf5871c79146ad1093fb80c7ae41f0f9
SaintEed <dsteed18@gmail.com>
2021-03-26 19:25:07 +0000

    Added sort of list data (lastName ascending)

--- thymeleaf-crud-db/src/main/java/com/deusnex/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleaf-crud-db/src/main/java/com/deusnex/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.deusnex.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.deusnex.springboot.thymeleafdemo.entity.Employee;
@@ -7,4 +9,8 @@ import com.deusnex.springboot.thymeleafdemo.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	//That's it....no code required
+	
+	// add a method to sort by last name
+	
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
