c32789c42380573c289a322fe66ce984522edfe5
SaintEed <dsteed18@gmail.com>
2021-03-26 15:35:25 +0000

    Connect to db via Employee Service and Employee Repository. Create list

--- thymeleaf-crud-db/src/main/java/com/deusnex/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleaf-crud-db/src/main/java/com/deusnex/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,9 +1,6 @@
 package com.deusnex.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,39 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.deusnex.springboot.thymeleafdemo.entity.Employee;
+import com.deusnex.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-	// load employee data
 	
-	private List<Employee> theEmployees;
+	private EmployeeService employeeService;
 	
-	@PostConstruct
-	private void loadData() {
-		
-		// create employees
-		Employee emp1 = new Employee(1, "Leslie", "Andrews", "lesley@email.com");
-		Employee emp2 = new Employee(2, "Emma", "Baumgarten", "emma@email.com");
-		Employee emp3 = new Employee(3, "Avani", "Gupta", "avani@email.com");
-		
-		
-		// create the list
-		theEmployees = new ArrayList<>();
-		
-		
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
-		
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
-	
 	
 	
 	//add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployees (Model theModel) {
+		
+		// get employees from the db
+		List<Employee> theEmployees = employeeService.findAll();
 		
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
